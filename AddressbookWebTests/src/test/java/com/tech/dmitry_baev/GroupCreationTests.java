df9bc7e992b6816dc7e62c94a0caa03ec1cea1ac
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-12-13 09:43:48 +0300

    Extracted methods from original code

--- AddressbookWebTests/src/test/java/com/tech/dmitry_baev/GroupCreationTests.java
+++ AddressbookWebTests/src/test/java/com/tech/dmitry_baev/GroupCreationTests.java
@@ -13,6 +13,8 @@ public class GroupCreationTests {
     @Before
     public void setUp() {
         driver = new ChromeDriver();
+        driver.get("http://localhost/addressbook/");
+        login();
     }
 
     @After
@@ -21,21 +23,43 @@ public class GroupCreationTests {
     }
 
     @Test
-    public void newGroup() {
-        driver.get("http://localhost/addressbook/");
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-        driver.findElement(By.linkText("groups")).click();
+    public void createGroup() {
+        gotoGroupCreation();
+        initGroupCreation();
+        fillGroupForm();
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    private void returnToGroupPage() {
+        driver.findElement(By.linkText("group page")).click();
+    }
+
+    private void submitGroupCreation() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    private void initGroupCreation() {
         driver.findElement(By.name("new")).click();
+    }
+
+    private void gotoGroupCreation() {
+        driver.findElement(By.linkText("groups")).click();
+    }
+
+    private void fillGroupForm() {
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).sendKeys("test1");
         driver.findElement(By.name("group_header")).click();
         driver.findElement(By.name("group_header")).sendKeys("test2");
         driver.findElement(By.name("group_footer")).click();
         driver.findElement(By.name("group_footer")).sendKeys("test3");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("group page")).click();
+    }
+
+    private void login() {
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
     }
 }
