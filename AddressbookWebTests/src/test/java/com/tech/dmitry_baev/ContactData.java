878bc1c377a786d4fef1d7272bbe7f0906520c7b
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-12-13 10:13:31 +0300

    New test for Contact creation from Selenium IDE and refactoring

---
+++ AddressbookWebTests/src/test/java/com/tech/dmitry_baev/ContactData.java
@@ -0,0 +1,31 @@
+package com.tech.dmitry_baev;
+
+public class ContactData {
+    private final String name;
+    private final String lastName;
+    private final String phoneNumber;
+    private final String email;
+
+    public ContactData(String name, String lastName, String phoneNumber, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
